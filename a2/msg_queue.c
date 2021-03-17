54a4c27b71b747f2f1dd79614364d0fab8faf203
helenzzq <helenzhu0411@gmail.com>
2021-03-17 05:02:38 -0400

    Finish part 1

--- a2/msg_queue.c
+++ a2/msg_queue.c
@@ -28,6 +28,20 @@
 #include "msg_queue.h"
 #include "ring_buffer.h"
 
+typedef struct event_thread
+{
+	list_entry ent;
+	mutex_t *poll_mutex; //poll_mutex pointer
+	cond_t *evt_wait;	 // condition variable for poll
+
+	bool stg_three_trigger; // Set to false when stage 3 is over
+
+	int evt;
+
+	msg_queue_t queue;
+
+} event_thread;
+
 // Message queue implementation backend
 typedef struct mq_backend
 {
@@ -274,25 +288,234 @@ int msg_queue_close(msg_queue_t *queue)
 	mutex_unlock(&(mq->hz_mutex));
 	return 0;
 }
+void subscribe_event(event_thread *data, size_t (*ptr)(ring_buffer *), int evt, int evt_flg, void *buffer, bool is_read)
+{
+	if (((ptr(buffer) && !is_read) || (is_read && ptr(buffer) < sizeof(size_t))) && (evt & evt_flg))
+	{
+		data->stg_three_trigger = true;
+		cond_signal(data->evt_wait);
+	}
+}
+int has_entry(bool is_read, list_entry *head, list_entry *next, mq_backend *mq)
+{
+	int handled_symbol = MQPOLL_WRITABLE;
+	if (!is_read)
+	{
+		handled_symbol = MQPOLL_READABLE;
+	}
+	if (next == head)
+	{
+		list_entry *root;
+		list_for_each(root, &(mq->hz_node))
+		{
+			event_thread *data = container_of(root, event_thread, ent);
 
+			mutex_lock(data->poll_mutex);
+			if (data->queue != MSG_QUEUE_NULL && data)
+			{
+				subscribe_event(data, ring_buffer_free, data->evt, is_read, &(mq->buffer), is_read);
+			}
+		}
+	}
+
+	mutex_unlock(&(mq->hz_mutex));
+
+	return -1;
+}
+
+/*Helper function for error checking when performing read and write action
+Return 0 if no error, return -1 if error occur.
+*/
+int check_empty_queue(msg_queue_t queue, int msg_q_rw)
+{
+	if (!queue || !(get_flags(queue) & msg_q_rw))
+	{
+		errno = EBADF;
+		if (msg_q_rw == MSG_QUEUE_READER)
+		{
+			report_error("msg_queue_read");
+		}
+		else
+		{
+			report_error("msg_queue_write");
+		}
+		return -1;
+	}
+	return 0;
+}
+/*
+Error checking for read & write operation
+Return true if error occurs
+*/
+bool err_checking_rw(bool is_read, int err_code, mutex_t *mutex_lk, bool condition)
+{
+	if (condition)
+	{
+		errno = err_code;
+		if (is_read)
+		{
+			report_error("msg_queue_read");
+		}
+		else
+		{
+			report_error("msg_queue_write");
+		}
+		mutex_unlock(mutex_lk);
+		return true;
+	}
+	return false;
+}
+
+bool check_buffer_availability(size_t (*ptr)(ring_buffer *), bool is_read, msg_queue_t queue, size_t msg_size, ring_buffer *buffer, mutex_t *mutex_lk, cond_t *occupied)
+{
+	bool err = false;
+	// Check if there's available space in the buffer
+	// If no space then wait
+	while ((is_read && ptr(buffer) == 0) || (!is_read && msg_size > ptr(buffer)))
+	{
+
+		if (get_flags(queue) & MSG_QUEUE_NONBLOCK)
+		{
+			err_checking_rw(true, EAGAIN, mutex_lk, true);
+			err = true;
+			break;
+		}
+		//Available space exist for read/write
+		cond_wait(occupied, mutex_lk);
+	}
+	return err;
+}
+
+/*	This function is triggered when an rb operation inolves unlocking
+Return true if unlock;
+*/
+bool unlock_op(mutex_t *mutex_lk, bool (*ptr)(ring_buffer *, void *, size_t), ring_buffer *rb, void *buffer, size_t length)
+{
+	if (!ptr(rb, buffer, length))
+	{
+		mutex_unlock(mutex_lk);
+		return true;
+	}
+	return false;
+}
+
+/*Get size of size_t 
+Common function for read & write
+*/
+size_t get_size(mutex_t *mutex_lk)
+{
+	mutex_lock(mutex_lk);
+	return sizeof(size_t);
+}
+
+/*
+Perform Read & write operation to the message and its length
+Brodcast the correponding cond_t to wake up all the threads
+that blocked on it
+*/
+int rw_msg(mq_backend *mq, bool is_read, size_t size, size_t length, void *buf, cond_t *occupied)
+{
+	size_t sizes[2] = {size, length};
+	void *buffers[2] = {&length, buf};
+	const void *buffers_const[2] = {(const void *)&length, (const void *)buf};
+	//Read the message and its length and unlock the lock correspondingly
+	for (int i = 0; i < 2; i++)
+	{
+		//If read/write fail, return -1;
+		if (is_read)
+		{
+			//Read the message and its length and unlock the lock correspondingly
+			if (unlock_op(&(mq->hz_mutex), ring_buffer_read, &(mq->buffer), buffers[i], sizes[i]))
+			{
+				return -1;
+			}
+		}
+		else
+		{
+			//Write the message and its length and unlock the lock correspondingly
+			if (!ring_buffer_write(&(mq->buffer), buffers_const[i], sizes[i]))
+			{
+				mutex_unlock(&(mq->hz_mutex));
+				return -1;
+			}
+		}
+	}
+	cond_broadcast(occupied);
+	return 0;
+}
 ssize_t msg_queue_read(msg_queue_t queue, void *buffer, size_t length)
 {
-	//TODO
-	(void)queue;
-	(void)buffer;
-	(void)length;
-	errno = ENOSYS;
-	return -1;
+	int err_code = check_empty_queue(queue, MSG_QUEUE_READER);
+	if (err_code != -1)
+	{
+		size_t size_msg;
+		mq_backend *mq = get_backend(queue);
+		size_t size = get_size(&(mq->hz_mutex));
+
+		//If there's no writer and the ring buffer is empty
+		//Return 0
+		if (mq->no_writers && ring_buffer_used(&(mq->buffer)) == 0)
+		{
+			mutex_unlock(&(mq->hz_mutex));
+			return 0;
+		}
+
+		//Update error code and check buffer availability
+		bool err = check_buffer_availability(ring_buffer_used, true, queue, 0, &(mq->buffer), &(mq->hz_mutex), &(mq->hz_write_occupied));
+		//Load the message length & Check whether that message is too long
+		if (err || unlock_op(&(mq->hz_mutex), ring_buffer_peek, &(mq->buffer), &size_msg, size) || err_checking_rw(true, EMSGSIZE, &(mq->hz_mutex), size_msg > length))
+		{
+			return -1;
+		}
+		//Read msg and corresponding length
+		if (rw_msg(mq, true, size, size_msg, buffer, &(mq->hz_read_occupied)) == -1)
+		{
+			return -1;
+		}
+		has_entry(true, &(mq->hz_node.head), mq->hz_node.head.next, mq);
+
+		return size_msg;
+	}
+	return err_code;
 }
 
 int msg_queue_write(msg_queue_t queue, const void *buffer, size_t length)
 {
-	//TODO
-	(void)queue;
-	(void)buffer;
-	(void)length;
-	errno = ENOSYS;
-	return -1;
+	int err = check_empty_queue(queue, MSG_QUEUE_WRITER);
+	if (err != -1)
+	{
+
+		mq_backend *mq = get_backend(queue);
+		size_t buf_size = (mq->buffer).size;
+		size_t msg_size = length + sizeof(size_t);
+		size_t size = buf_size - length - get_size(&(mq->hz_mutex));
+		//There are 3 error checking that need to perform
+		//1. Write is valid or not 2. Message size > buffer size or not
+		//3. Whether all write handler are closed
+		int err_code[3] = {EINVAL, EMSGSIZE, EPIPE};
+		bool conditions[3] = {length == 0, size < 0, mq->no_readers};
+		for (int i = 0; i < 3; i++)
+		{
+			if (err_checking_rw(false, err_code[i], &(mq->hz_mutex), conditions[i]))
+			{
+				return -1;
+			}
+		}
+		//Update error code and check buffer availability
+		bool err = check_buffer_availability(ring_buffer_free, false, queue, msg_size, &(mq->buffer), &(mq->hz_mutex), &(mq->hz_read_occupied));
+		if (err)
+		{
+			return -1;
+		}
+		if (rw_msg(mq, false, sizeof(size_t), length, (void *)buffer, &(mq->hz_write_occupied)) == -1)
+		{
+			return -1;
+		}
+
+		has_entry(false, &(mq->hz_node.head), mq->hz_node.head.next, mq);
+		err = 0;
+	}
+	return err;
 }
 
 int msg_queue_poll(msg_queue_pollfd *fds, size_t nfds)
